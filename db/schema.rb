f03ff47c43e352b21307f258432337169054b222
Albertino Francisco <albertino@ua.pt>
2020-07-07 10:53:10 +0100

    Migrate database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_055556) do
+ActiveRecord::Schema.define(version: 2020_07_07_081714) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
